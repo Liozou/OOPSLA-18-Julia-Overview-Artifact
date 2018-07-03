24666fb123527d5aa33553292531b665c5192188
Ben Chung <bchung@ccs.neu.edu>
2018-07-02 22:21:24 -0400

    Changes...

--- julia/analytics/make_figures.sh
+++ julia/analytics/make_figures.sh
@@ -1,1 +1,1 @@
-../usr/bin/julia -e "include(\"\$JULIA_HOME/../../analytics/collect_data.jl\"); set_logs_dir(\"\$JULIA_HOME/../../logs/\"); include(\"\$JULIA_HOME/../../analytics/generate_csv.jl\")"
+../julia -e "include(\"\$JULIA_HOME/../../analytics/collect_data.jl\"); set_logs_dir(\"\$JULIA_HOME/../../logs/\"); include(\"\$JULIA_HOME/../../analytics/generate_csv.jl\")"
